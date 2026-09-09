2452bbc0ce1c196be327ca2edc8c4ed16dfd0eb1
sankaranarayanan1409 <bsridharsankar@gmail.com>
2026-09-09 18:51:59 +0530

    added scroll

--- app/src/main/java/com/example/monitrack/ui/track/ActivityPage.kt
+++ app/src/main/java/com/example/monitrack/ui/track/ActivityPage.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -49,6 +51,7 @@ fun ActivityPage(
     Column(
         modifier = modifier
             .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
             .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp),
